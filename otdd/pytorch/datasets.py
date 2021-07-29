Added Gradient Flows, fixed bug with symmetric_tasks=True

otdd/pytorch/datasets.py
@@ -21,19 +21,31 @@ import torchvision.datasets as dset
 
 import torchtext
 from torchtext.data.utils import get_tokenizer
-import sentence_transformers as st
 
 import h5py
 
-from .. import ROOT_DIR, HOME_DIR
+from .. import DATA_DIR
 
 from .utils import interleave, process_device_arg, random_index_split, \
                    spectrally_prescribed_matrix, rot, rot_evecs
 
+from .sqrtm import create_symm_matrix
 
 logger = logging.getLogger(__name__)
 
 
+DATASET_NCLASSES = {
+    'MNIST': 10,
+    'FashionMNIST': 10,
+    'EMNIST': 26,
+    'KMNIST': 10,
+    'USPS': 10,
+    'CIFAR10': 10,
+    'SVHN': 10,
+    'STL10': 10,
+    'LSUN': 10,
+    'tiny-ImageNet': 200
+}
 
 DATASET_SIZES = {
     'MNIST': (28,28),
@@ -42,6 +54,10 @@ DATASET_SIZES = {
     'QMNIST': (28,28),
     'KMNIST': (28,28),
     'USPS': (16,16),
+    'SVHN': (32, 32),
+    'CIFAR10': (32, 32),
+    'STL10': (96, 96),
+    'tiny-ImageNet': (64,64)
 }
 
 DATASET_NORMALIZATION = {
@@ -52,6 +68,7 @@ DATASET_NORMALIZATION = {
     'EMNIST' : ((0.1307,), (0.3081,)),
     'KMNIST' : ((0.1307,), (0.3081,)),
     'ImageNet': ((0.485, 0.456, 0.406),(0.229, 0.224, 0.225)),
+    'tiny-ImageNet': ((0.485, 0.456, 0.406),(0.229, 0.224, 0.225)),
     'CIFAR10': ((0.485, 0.456, 0.406),(0.229, 0.224, 0.225)),
     'CIFAR100': ((0.485, 0.456, 0.406),(0.229, 0.224, 0.225)),
     'STL10': ((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
@@ -115,18 +132,20 @@ class SubsetSampler(torch.utils.data.Sampler):
 
 class CustomTensorDataset(torch.utils.data.Dataset):
     """TensorDataset with support of transforms."""
-    def __init__(self, tensors, transform=None):
+    def __init__(self, tensors, transform=None, target_transform=None):
         assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors)
         self.tensors = tensors
         self.transform = transform
+        self.target_transform = target_transform
 
     def __getitem__(self, index):
         x = self.tensors[0][index]
-
         if self.transform:
             x = self.transform(x)
 
         y = self.tensors[1][index]
+        if self.target_transform:
+            y = self.target_transform(y)
 
         return x, y
 
@@ -224,7 +243,7 @@ def make_gmm_dataset(config='random', classes=10,dim=2,samples=10,spread = 1,
 
     """
     means, covs, distribs = [], [], []
-    _configd = gmm_configs[config]
+    _configd = None if config == 'random' else gmm_configs[config]
     spread = spread if (config == 'random' or not 'spread' in _configd) else _configd['spread']
     shift  = shift if (config == 'random' or not 'shift' in _configd) else _configd['shift']
 
@@ -254,8 +273,7 @@ def make_gmm_dataset(config='random', classes=10,dim=2,samples=10,spread = 1,
         X += torch.tensor(shift)
 
     if shuffle:
-        indx = torch.arange(Y.shape[0])
-        print(indx)
+        idxs = torch.randperm(Y.shape[0])
         X = X[idxs, :]
         Y = Y[idxs]
     return X, Y, distribs
@@ -276,7 +294,6 @@ def load_torchvision_data(dataname, valid_size=0.1, splits=None, shuffle=True,
             transform_dataname = dataname
         else:
             transform_dataname = 'ImageNet'
-
 
         transform_list = []
 
@@ -307,11 +324,11 @@ def load_torchvision_data(dataname, valid_size=0.1, splits=None, shuffle=True,
     if data is None:
         DATASET = getattr(torchvision.datasets, dataname)
         if datadir is None:
-            datadir = os.path.join(ROOT_DIR,'data/')
+            datadir = DATA_DIR
         if dataname == 'EMNIST':
             split = 'letters'
-            train = DATASET(datadir, split=split, train=True, download=True, transform=train_transform)
-            test = DATASET(datadir, split=split, train=False, download=True, transform=valid_transform)
+            train = DATASET(datadir, split=split, train=True, download=download, transform=train_transform)
+            test = DATASET(datadir, split=split, train=False, download=download, transform=valid_transform)
             ## EMNIST seems to have a bug - classes are wrong
             _merged_classes = set(['C', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'S', 'U', 'V', 'W', 'X', 'Y', 'Z'])
             _all_classes = set(list(string.digits + string.ascii_letters))
@@ -329,15 +346,25 @@ def load_torchvision_data(dataname, valid_size=0.1, splits=None, shuffle=True,
                 train.targets -= 1
                 test.targets -= 1
         elif dataname == 'STL10':
-            train = DATASET(datadir, split='train', download=True, transform=train_transform)
-            test = DATASET(datadir, split='test', download=True, transform=valid_transform)
+            train = DATASET(datadir, split='train', download=download, transform=train_transform)
+            test = DATASET(datadir, split='test', download=download, transform=valid_transform)
             train.classes = ['airplane', 'bird', 'car', 'cat', 'deer', 'dog', 'horse', 'monkey', 'ship', 'truck']
             test.classes = train.classes
             train.targets = torch.tensor(train.labels)
             test.targets = torch.tensor(test.labels)
+        elif dataname == 'SVHN':
+            train = DATASET(datadir, split='train', download=download, transform=train_transform)
+            test = DATASET(datadir, split='test', download=download, transform=valid_transform)
+            ## In torchvision, SVHN 0s have label 0, not 10
+            train.classes = test.classes = [str(i) for i in range(10)]
+            train.targets = torch.tensor(train.labels)
+            test.targets = torch.tensor(train.labels)
+        elif dataname == 'LSUN':
+            pdb.set_trace()
+            train = DATASET(datadir, classes='train', download=download, transform=train_transform)
         else:
-            train = DATASET(datadir, train=True, download=True, transform=train_transform)
-            test = DATASET(datadir, train=False, download=True, transform=valid_transform)
+            train = DATASET(datadir, train=True, download=download, transform=train_transform)
+            test = DATASET(datadir, train=False, download=download, transform=valid_transform)
     else:
         train, test = data
 
@@ -368,6 +395,16 @@ def load_torchvision_data(dataname, valid_size=0.1, splits=None, shuffle=True,
         elif type(splits) in [list, np.ndarray]:
             snames = ['split_{}'.format(i) for i in range(len(splits))]
             slens  = splits
+        slens = np.array(slens)
+        if any(slens < 0): # Split expressed as -1, i.e., 'leftover'
+            assert sum(slens < 0) == 1, 'Can only deal with one split being -1'
+            idx_neg = np.where(slens == -1)[0][0]
+            slens[idx_neg] = len(train) - np.array([x for x in slens if x > 0]).sum()
+        elif slens.sum() > len(train):
+            logging.warning("Not enough samples to satify splits..cropping train...")
+            if 'train' in snames:
+                slens[snames.index('train')] = len(train) - slens[np.array(snames) != 'train'].sum()
+
         idxs = np.arange(len(train))
         if not stratified:
             np.random.shuffle(idxs)
@@ -417,29 +454,35 @@ def load_torchvision_data(dataname, valid_size=0.1, splits=None, shuffle=True,
     return fold_loaders, {'train': train, 'test':test}
 
 
-def load_imagenet(datadir=None, resize=None, tiny=False, **kwargs):
+def load_imagenet(datadir=None, resize=None, tiny=False, augmentations=False, **kwargs):
     """ Load ImageNet dataset """
-    print(datadir)
     if datadir is None and (not tiny):
-        datadir = os.path.join(HOME_DIR,'datasets/imagenet')
+        datadir = os.path.join(DATA_DIR,'imagenet')
     elif datadir is None and tiny:
-        datadir = os.path.join(HOME_DIR,'datasets/tiny-imagenet-200')
-
+        datadir = os.path.join(DATA_DIR,'tiny-imagenet-200')
 
     traindir = os.path.join(datadir, "train")
     validdir = os.path.join(datadir, "val")
-    train_transform_list = [
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(
-            brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2
-        ),
-        transforms.ToTensor(),
-        transforms.Normalize(*DATASET_NORMALIZATION['ImageNet'])
-    ]
+    if augmentations:
+        train_transform_list = [
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(
+                brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2
+            ),
+            transforms.ToTensor(),
+            transforms.Normalize(*DATASET_NORMALIZATION['ImageNet'])
+        ]
+    else:
+        train_transform_list = [
+            transforms.Resize(224), # revert back to 256
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(*DATASET_NORMALIZATION['ImageNet'])
+        ]
 
     valid_transform_list = [
-        transforms.Resize(256),
+        transforms.Resize(224),# revert back to 256
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(*DATASET_NORMALIZATION['ImageNet'])
@@ -464,7 +507,6 @@ def load_imagenet(datadir=None, resize=None, tiny=False, **kwargs):
             valid_transform_list
         ),
     )
-
     fold_loaders, dsets = load_torchvision_data('Imagenet', transform=[],
                                                 data=(train_data, valid_data),
                                                 **kwargs)
@@ -528,8 +570,8 @@ def load_textclassification_data(dataname, vecname='glove.42B.300d', shuffle=Tru
 
     debug = False
 
-    dataroot = '/tmp/' if debug else os.path.join(ROOT_DIR, 'data')
-    veccache = os.path.join(ROOT_DIR,'data','.vector_cache')
+    dataroot = '/tmp/' if debug else DATA_DIR #os.path.join(ROOT_DIR, 'data')
+    veccache = os.path.join(dataroot,'.vector_cache')
 
     if loading_method == 'torchtext':
         ## TextClassification object datasets already do word to token mapping inside.
@@ -559,6 +601,7 @@ def load_textclassification_data(dataname, vecname='glove.42B.300d', shuffle=Tru
         else:
             batch_processor = partial(batch_processor_tt,TEXT=text_field,return_lengths=True)
     elif loading_method == 'sentence_transformers':
+        import sentence_transformers as st
         dpath  = os.path.join(dataroot,TEXTDATA_PATHS[dataname])
         reader = st.readers.LabelSentenceReader(dpath)
         if embedding_model is None:

otdd/pytorch/distance.py
@@ -2,9 +2,9 @@
 
 Throught this module, source and target are often used to refer to the two datasets
 being compared. This notation is legacy from NLP, and does not carry other particular
-meaning, e.g., the distance is nevertheless symmetric to the order of D1/D2. The
-reason for this notation is that here X and Y are usually reserved to distinguish
-between features and labels.
+meaning, e.g., the distance is nevertheless symmetric (though not always identical -
+due to stochsticity in the computation) to the order of D1/D2. The reason for this
+notation is that here X and Y are usually reserved to distinguish between features and labels.
 
 Other important notation:
     X1, X2: feature tensors of the two datasets
@@ -32,13 +32,14 @@ from matplotlib import cm
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+from sklearn.cluster import k_means, DBSCAN
 
 ## Local Imports
 from ..plotting import heatmap, gaussian_density_plot, imshow_group_boundaries
 from .utils import load_full_dataset, augmented_dataset, extract_data_targets
 from .moments import compute_label_stats
 from .wasserstein import efficient_pwdist_gauss, pwdist_exact, pwdist_upperbound, pwdist_means_only
-from .utils import register_gradient_hook, process_device_arg, multiclass_hinge_loss
+from .utils import register_gradient_hook, process_device_arg, multiclass_hinge_loss, load_full_dataset
 
 
 import matplotlib
@@ -77,14 +78,16 @@ class DatasetDistance():
         method (str): if set to 'augmentation', the covariance matrix will be
             approximated and appended to each point, if 'precomputed_labeldist',
             the label-to-label distance is computed exactly in advance.
-        symmetric_tasks (bool): whether the two underlying datasets are the same
-            (e.g., when computing distance between subsets of classes).
+        symmetric_tasks (bool): whether the two underlying datasets are the same.
+            If true, will save some computation.
         feature_cost (str or callable): if not 'euclidean', must be a callable
             that implements a cost function between feature vectors.
         src_embedding (callable, optional): if provided, source data will be
             embedded using this function prior to distance computation.
         tgt_embedding (callable, optional): if provided, target data will be
             embedded using this function prior to distance computation.
+        ignore_source_labels (bool): for unsupervised computation of distance
+        ignore_target_labels (bool): for unsupervised computation of distance
         loss (str): loss type to be passed to samples_loss. only 'sinkhorn' is
             accepted for now.
         debiased_loss (bool): whether to use the debiased version of sinkhorn.
@@ -143,6 +146,8 @@ class DatasetDistance():
                  feature_cost='euclidean',
                  src_embedding=None,
                  tgt_embedding=None,
+                 ignore_source_labels=False,
+                 ignore_target_labels=False,
                  ## Outer OT (dataset to dataset) problem arguments
                  loss='sinkhorn', debiased_loss=False, p=2, entreg=0.1,
                  λ_x=1.0, λ_y=1.0,
@@ -177,9 +182,11 @@ class DatasetDistance():
         self.entreg = entreg
         self.loss = loss
         self.debiased_loss = debiased_loss
+        self.feature_cost = feature_cost
         self.src_embedding = src_embedding
         self.tgt_embedding = tgt_embedding
-        self.feature_cost = feature_cost
+        self.ignore_source_labels = ignore_source_labels
+        self.ignore_target_labels = ignore_target_labels
         self.λ_x = λ_x
         self.λ_y = λ_y
         ## For inner (label) OT problem - only used if gaussian approx is False
@@ -240,30 +247,81 @@ class DatasetDistance():
         self.tgt_embedding = None
 
 
+    def _load_infer_labels(self, D, classes=None, reindex=None, reindex_start=None):
+
+        if classes:
+            k = len(classes)
+            labeling_fun = lambda X: torch.LongTensor(k_means(X.numpy(), k)[1])
+        else:
+            labeling_fun = lambda X: torch.LongTensor(DBSCAN(eps=5, min_samples = 4).fit(X).labels_)
+
+        X, Y_infer, Y_true = load_full_dataset(D, targets='infer',
+                                 min_labelcount=self.min_labelcount,
+                                 labeling_function = labeling_fun,
+                                 return_both_targets=True,
+                                 force_label_alignment=True,
+                                 reindex=reindex, reindex_start=reindex_start)
+
+        return X, Y_infer, Y_true
+
+
     def _init_data(self, D1, D2):
         """ Preprocessing of datasets. Extracts value and coding for effective
         (i.e., those actually present in sampled data) class labels.
         """
+
         targets1, classes1, idxs1 = extract_data_targets(D1)
         targets2, classes2, idxs2 = extract_data_targets(D2)
-        self.targets1, self.targets2 = targets1, targets2
-        self.idxs1, self.idxs2 = idxs1, idxs2
 
-        ## Effective dataset size
+        ## Get effective dataset number of samples
+        self.idxs1, self.idxs2 = idxs1, idxs2
         self.n1 = len(self.idxs1)
         self.n2 = len(self.idxs2)
+
+        if (targets1 is None) or self.ignore_source_labels: # Unsupervised setting
+            X, Y_infer, Y_true = self._load_infer_labels(D1, classes1, reindex=True, reindex_start=0)
+            self.targets1 = targets1 = Y_infer
+            self.X1, self.Y1 = X, Y_infer
+            if Y_true is not None: self.Y1_true = Y_true # will not be used by OTDD, stored only for downstream eval
+        else:
+            self.targets1 = targets1
 
         ## Effective classes seen in data (idxs here needed to be filtered
         ## in case dataloader has subsampler)
         ## Indices of classes (might be different from class ids!)
-        vals1, cts1 = torch.unique(targets1[idxs1], return_counts=True)
-        vals2, cts2 = torch.unique(targets2[idxs2], return_counts=True)
+        if (targets1 is None) or self.ignore_source_labels:
+            vals1, cts1 = torch.unique(targets1, return_counts=True)
+        else:
+            vals1, cts1 = torch.unique(targets1[idxs1], return_counts=True)
 
         ## Ignore everything with a label occurring less than k times
         self.V1 = torch.sort(vals1[cts1 >= self.min_labelcount])[0]
+
+        if (targets2 is None) or self.ignore_target_labels:
+            reindex_start = len(self.V1) if (self.loss == 'sinkhorn' and self.debiased_loss) else True
+            X, Y_infer, Y_true = self._load_infer_labels(D2, classes2, reindex=True, reindex_start=reindex_start)
+            self.targets2 = targets2 = Y_infer - reindex_start
+            assert self.targets2.min() == 0
+            self.X2, self.Y2 = X, Y_infer
+            if Y_true is not None: self.Y2_true = Y_true
+        else:
+            self.targets2 = targets2
+
+        ## Effective classes seen in data (idxs here needed to be filtered
+        ## in case dataloader has subsampler)
+        ## Indices of classes (might be different from class ids!)
+        if (targets2 is None) or self.ignore_target_labels:
+            vals2, cts2 = torch.unique(targets2, return_counts=True)
+        else:
+            vals2, cts2 = torch.unique(targets2[idxs2], return_counts=True)
+
+        ## Ignore everything with a label occurring less than k times
         self.V2 = torch.sort(vals2[cts2 >= self.min_labelcount])[0]
+
+
         self.classes1 = [classes1[i] for i in self.V1]
         self.classes2 = [classes2[i] for i in self.V2]
+
 
         if self.method == 'jdot': ## JDOT only works if same labels on both datasets
             assert torch.all(self.V1 == self.V2)
@@ -320,29 +378,22 @@ class DatasetDistance():
 
         if self.loss == 'sinkhorn' and self.debiased_loss:
             ## We will need to relabel targets {0,...,n-1} and {n,...,n-m-1}
-            self.X1, self.Y1 = load_full_dataset(self.D1, targets=True,
-                                                 labels_keep=self.V1,
-                                                 maxsamples=maxsamples,
-                                                 device=device,
-                                                 dtype=dtype,
-                                                 reindex=range(len(self.V1)))
-            if self.symmetric_tasks:
-                self.X2, self.Y2 = self.X1, self.Y1
-            else:
-                self.X2, self.Y2 = load_full_dataset(self.D2, targets=True,
-                                                     labels_keep=self.V2,
-                                                     maxsamples=maxsamples,
-                                                     device=device,
-                                                     dtype=dtype,
-                                                     reindex=range(len(self.V1), len(self.V1) + len(self.V2)))
+            reindex_start_d2 = len(self.V1)
         else:
             ## Suffices to relabel targets {0,...,n-1} and {0,...,m-1}
+            reindex_start_d2 = 0
+
+        if self.X1 is None or self.Y1 is None:
+            assert not self.ignore_source_labels, 'Should not be here if igoring target labels'
             self.X1, self.Y1 = load_full_dataset(self.D1, targets=True,
                                                  labels_keep=self.V1,
                                                  maxsamples=maxsamples,
                                                  device=device,
                                                  dtype=dtype,
-                                                 reindex=True)
+                                                 reindex=True,
+                                                 reindex_start = 0)
+        if self.X2 is None or self.Y2 is None:
+            assert not self.ignore_target_labels, 'Should not be here if igoring target labels'
             if self.symmetric_tasks:
                 self.X2, self.Y2 = self.X1, self.Y1
             else:
@@ -351,7 +402,9 @@ class DatasetDistance():
                                                      maxsamples=maxsamples,
                                                      device=device,
                                                      dtype=dtype,
-                                                     reindex=True)
+                                                     reindex=True,
+                                                     reindex_start = reindex_start_d2)
+
 
         logger.info("Full datasets sizes")
         logger.info(" * D1 = {} x {} ({} unique labels)".format(*
@@ -484,7 +537,7 @@ class DatasetDistance():
                 elif self.inner_ot_method == 'means_only':
                     DYY1 = pwdist(Means[0])
                     DYY1_means = DYY1
-                else:
+                else: # Exact
                     DYY1 = pwdist(self.X1, self.Y1)
             else:
                 if self.inner_ot_method == 'gaussian_approx':
@@ -503,7 +556,7 @@ class DatasetDistance():
                 elif self.inner_ot_method == 'means_only':
                     DYY2 = pwdist(Means[1])
                     DYY2_means = DYY2
-                else:
+                else: # Exact
                     DYY2 = pwdist(self.X2, self.Y2)
             else:
                 logger.info('Found pre-existing D2 label-label stats, will not recompute')
@@ -515,6 +568,7 @@ class DatasetDistance():
                     DYY2 = self._pwlabel_stats_2['dlabs']
         else:
             sqrtΣ1, sqrtΣ2 = None, None  # Will have to compute during cross
+            DYY1 = DYY2 = None # we don't need these if debiased_loss=False
 
         ## Compute Cross-Distances
         logger.info('Pre-computing pairwise label Wasserstein distances D1 <-> D2...')
@@ -532,7 +586,7 @@ class DatasetDistance():
 
         if self.debiased_loss and self.symmetric_tasks:
             ## In this case we can reuse DXY to get DYY1 and DYY
-            DYY1, DYY2 = DXY, DXY
+            DYY1, DYY2 = DYY12, DYY12
             if self.inner_ot_method in ['gaussian_approx', 'naive_upperbound', 'means_only']:
                 DYY1_means, DYY2_means = DXY_means, DXY_means
 
@@ -546,7 +600,6 @@ class DatasetDistance():
             D = DYY12
             if self.inner_ot_method in ['gaussian_approx', 'naive_upperbound', 'means_only']:
                 D_means = DYY12_means
-
 
         ## Collect and save
         self.label_distances  = D
@@ -584,7 +637,8 @@ class DatasetDistance():
 
         """
         device_dists = self.device
-        if (self.n1 > 5000 or self.n2 > 5000) and maxsamples > 1000 and self.device != 'cpu':
+        GPU_LIMIT = 10000
+        if (self.n1 > GPU_LIMIT or self.n2 > GPU_LIMIT) and maxsamples > GPU_LIMIT and self.device != 'cpu':
             logger.warning('Warning: maxsamples = {} > 5000, and device = {}. Loaded data' \
                    ' might not fit in GPU. Computing distances on' \
                    ' CPU.'.format(maxsamples, self.device))